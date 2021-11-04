d70e52403ee633b6f934e10cbb56b61c3e0229cb
Reaga21 <andrea.robitzsch@gmail.com>
2021-11-04 10:12:15 +0100

    added list view to registration page

--- lib/src/views/registration_page.dart
+++ lib/src/views/registration_page.dart
@@ -26,149 +26,150 @@ class _RegistrationState extends State<Registration> {
       ),
       body: Form(
         key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 20.0),
-                  child: Text(
-                    "Registrierung",
+        child: ListView(
+          padding: const EdgeInsets.all(8),
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20.0),
+                    child: Text(
+                      "Registrierung",
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Leg einen Account an",
                     style: TextStyle(
-                      fontSize: 30,
+                      fontSize: 20,
                       color: Theme.of(context).primaryColor,
                     ),
                     textAlign: TextAlign.center,
                   ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Leg einen Account an",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 8.0),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width/1.5,
-                    height: 50.0,
-                    child: TextField(
-                      controller: bNController,
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-
-                          hintText: 'Benutzername'),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width/1.5,
-                    height: 50.0,
-                    child: TextFormField(validator: (value) {
-                      if(value!.isEmpty){
-                        return 'Bitte gib eine gültige Email-Adresse ein.';
-                      }
-                      return null;
-
-                    } ,
-                      controller: eMailController,
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Email Adresse'),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width/1.5,
-                    height: 50.0,
-                    child: TextField(
-                      controller: pWController,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Passwort'),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width/1.5,
-                    height: 50.0,
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        errorMaxLines: 3,
-                          border: OutlineInputBorder(),
-                          hintText: 'Passwort bestätigen'
-                      ),
-                      validator: (value){
-                        if(pWController.text != value)
-                          {return "Die beiden Passwörter sind ungleich";
-                          }
-                        return null;
-                      }
-                    ),
-                  ),
-                ),
-              ],
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                height: 50,
+                ],
               ),
-              ElevatedButton(
-                child: const Text('Registrieren'),
-                style: ElevatedButton.styleFrom(
-                    primary: Theme.of(context).backgroundColor),
-                onPressed: () {
-                 if (_formKey.currentState!.validate()) {
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
 
-                      Navigator.pushReplacement(context,
-                          MaterialPageRoute(builder: (_) => const HomePage()));
-                    }
-                 },
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 8.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width/1.5,
+                      height: 50.0,
+                      child: TextField(
+                        controller: bNController,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+
+                            hintText: 'Benutzername'),
+                      ),
+                    ),
                   ),
-              ],
-            ),
-          ],
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width/1.5,
+                      height: 50.0,
+                      child: TextFormField(validator: (value) {
+                        if(value!.isEmpty){
+                          return 'Bitte gib eine gültige Email-Adresse ein.';
+                        }
+                        return null;
+
+                      } ,
+                        controller: eMailController,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Email Adresse'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width/1.5,
+                      height: 50.0,
+                      child: TextField(
+                        controller: pWController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Passwort'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width/1.5,
+                      height: 50.0,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          errorMaxLines: 3,
+                            border: OutlineInputBorder(),
+                            hintText: 'Passwort bestätigen'
+                        ),
+                        validator: (value){
+                          if(pWController.text != value)
+                            {return "Die beiden Passwörter sind ungleich";
+                            }
+                          return null;
+                        }
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton(
+                  child: const Text('Registrieren'),
+                  style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).backgroundColor),
+                  onPressed: () {
+                   if (_formKey.currentState!.validate()) {
+
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (_) => const HomePage()));
+                      }
+                   },
+                    ),
+                ],
+              ),
+            ],
+
         ),
       ),
     );
