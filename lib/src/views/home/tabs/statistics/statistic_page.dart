e35e820732acfb7856fb81e3fc030215f89bad95
andrea.robitzsch <andrea.robitzsch@student.fh-kiel.de>
2022-01-03 18:37:59 +0100

    work in progress statistic page

--- lib/src/views/home/tabs/statistics/statistic_page.dart
+++ lib/src/views/home/tabs/statistics/statistic_page.dart
@@ -16,17 +16,27 @@ class StatisticPage extends StatefulWidget {
 }
 
 class _StatisticPageState extends State<StatisticPage> {
+  late List<StepsData> _chartData;
+
+  @override
+  void initState() {
+    _chartData = getChartData();
+    super.initState();
+  }
+
   Future<Map<String, dynamic>> readData() async {
     CollectionReference steps = FirebaseFirestore.instance.collection('steps');
     DocumentSnapshot date =
-    await steps.doc(FirebaseAuth.instance.currentUser!.uid).get();
+        await steps.doc(FirebaseAuth.instance.currentUser!.uid).get();
     Map<String, dynamic> data1 = date.data() as Map<String, dynamic>;
     Map<String, dynamic> dataSteps = data1['steps'] as Map<String, dynamic>;
 
     return dataSteps;
   }
 
-  final List<String> days7 = [
+
+
+  /* final List<String> days7 = [
     DateFormat('yyyy-MM-dd')
         .format(DateTime.now().subtract(const Duration(days: 6))),
     DateFormat('yyyy-MM-dd')
@@ -41,17 +51,14 @@ class _StatisticPageState extends State<StatisticPage> {
         .format(DateTime.now().subtract(const Duration(days: 1))),
     DateFormat('yyyy-MM-dd')
         .format(DateTime.now()),
-  ];
+  ];*/
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Sportify"),
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .primary,
+        backgroundColor: Theme.of(context).colorScheme.primary,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -62,15 +69,22 @@ class _StatisticPageState extends State<StatisticPage> {
               "Statistics",
               style: TextStyle(
                 fontSize: 30,
-                color: Theme
-                    .of(context)
-                    .colorScheme
-                    .secondary,
+                color: Theme.of(context).colorScheme.secondary,
               ),
               textAlign: TextAlign.center,
             ),
           ),
-          FutureBuilder(
+          SfCartesianChart(
+            series: <ChartSeries>[
+              BarSeries<StepsData, dynamic>(
+                  dataSource: _chartData,
+                  xValueMapper: (StepsData data,_) => data.date,
+                  yValueMapper: (StepsData data,_) => data.steps),
+            ],
+            primaryXAxis: CategoryAxis(),
+          ),
+
+          /*FutureBuilder(
               future: readData(),
               builder: (BuildContext context,
                   AsyncSnapshot<Map<String, dynamic>> dataSteps) {
@@ -98,18 +112,50 @@ class _StatisticPageState extends State<StatisticPage> {
 
               }
 
-          )
+          )*/
         ],
       ),
     );
+  }
 
+  List<StepsData> getChartData() {
+    final List<StepsData> chartData = [
+      StepsData(
+          DateFormat('yyyy-MM-dd')
+              .format(DateTime.now().subtract(const Duration(days: 6))),
+          1000),
+      StepsData(
+          DateFormat('yyyy-MM-dd')
+              .format(DateTime.now().subtract(const Duration(days: 5))),
+          1000),
+      StepsData(
+          DateFormat('yyyy-MM-dd')
+              .format(DateTime.now().subtract(const Duration(days: 4))),
+          1000),
+      StepsData(
+          DateFormat('yyyy-MM-dd')
+              .format(DateTime.now().subtract(const Duration(days: 3))),
+          1000),
+      StepsData(
+          DateFormat('yyyy-MM-dd')
+              .format(DateTime.now().subtract(const Duration(days: 2))),
+          1000),
+      StepsData(
+          DateFormat('yyyy-MM-dd')
+              .format(DateTime.now().subtract(const Duration(days: 1))),
+          1000),
+      StepsData(
+          DateFormat('yyyy-MM-dd')
+              .format(DateTime.now().subtract(const Duration(days: 0))),
+          1000),
+    ];
+    return chartData;
   }
 }
 
+class StepsData {
+  StepsData(this.date, this.steps);
 
-class _SalesData {
-  _SalesData(this.year, this.sales);
-
-  final String year;
-  final double sales;
+  final String date;
+  final int steps;
 }
