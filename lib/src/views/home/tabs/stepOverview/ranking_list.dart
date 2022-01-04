e855af15bdfb647cbeb1f2658cfb18e74520b26e
aeropagz <klaas.pelzer@gmx.de>
2022-01-04 18:07:17 +0100

    added pics for ranking list

--- lib/src/views/home/tabs/stepOverview/ranking_list.dart
+++ lib/src/views/home/tabs/stepOverview/ranking_list.dart
@@ -1,4 +1,5 @@
 import 'dart:collection';
+import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -14,10 +15,10 @@ class RankingList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final UserModel myUser = context.watch<UserModel>();
-    return StreamBuilder<Map<String, StepModel>>(
+    return StreamBuilder<Map<String, dynamic>>(
       stream: getFriendsStream(myUser.friends),
-      builder: (BuildContext context,
-          AsyncSnapshot<Map<String, StepModel>> snapshot) {
+      builder:
+          (BuildContext context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
         List<Widget> children;
         if (snapshot.hasData) {
           if (snapshot.data!.isEmpty) {
@@ -51,18 +52,20 @@ class RankingList extends StatelessWidget {
     );
   }
 
-  List<Widget> buildList(Map<String, StepModel> steps) {
+  List<Widget> buildList(Map<String, dynamic> data) {
     List<Widget> tiles = [];
+    Map<String, StepModel> steps = data["steps"] as Map<String, StepModel>;
+    Map<String, String> pics = data["pics"] as Map<String, String>;
 
-    var sortedKeys = steps.keys.toList(growable: false)
-      ..sort((k1, k2) =>
-          steps[k1]!.getTodaySteps().compareTo(steps[k2]!.getTodaySteps()));
-    LinkedHashMap sortedSteps = LinkedHashMap.fromIterable(sortedKeys.reversed,
-        key: (k) => k, value: (k) => steps[k]);
+    LinkedHashMap<dynamic, dynamic> sortedSteps = sortSteps(steps);
 
     sortedSteps.forEach((uid, steps) {
+      String ?pic = pics[uid];
       tiles.add(Card(
         child: ListTile(
+          trailing: CircleAvatar(
+            backgroundImage: MemoryImage(base64Decode(pic ?? "")),
+          ),
           title: Text("${steps.getTodaySteps()} Steps today"),
           subtitle: Text(steps.username),
         ),
@@ -71,17 +74,34 @@ class RankingList extends StatelessWidget {
     return tiles;
   }
 
-  Stream<Map<String, StepModel>> getFriendsStream(List<String> uids) async* {
+  LinkedHashMap<dynamic, dynamic> sortSteps(Map<String, StepModel> steps) {
+    var sortedKeys = steps.keys.toList(growable: false)
+      ..sort((k1, k2) =>
+          steps[k1]!.getTodaySteps().compareTo(steps[k2]!.getTodaySteps()));
+    LinkedHashMap sortedSteps = LinkedHashMap.fromIterable(sortedKeys.reversed,
+        key: (k) => k, value: (k) => steps[k]);
+    return sortedSteps;
+  }
+
+  Stream<Map<String, dynamic>> getFriendsStream(List<String> uids) async* {
     while (true) {
-      var result = {for (String uid in uids) uid: await getSteps(uid)};
-      yield result;
+      final steps = {for (String uid in uids) uid: await getSteps(uid)};
+      final pics = {for (String uid in uids) uid: await getPic(uid)};
+      yield {"steps": steps, "pics": pics};
       await Future.delayed(const Duration(seconds: 15));
     }
   }
 
+  Future<String> getPic(String uid) async {
+    final users = FirebaseFirestore.instance.collection("users");
+    final user = await users.doc(uid).get();
+    final pic = user.get("pic");
+    return pic;
+  }
+
   Future<StepModel> getSteps(String uid) async {
-    final CollectionReference steps =
-        FirebaseFirestore.instance.collection('steps');
+    final steps = FirebaseFirestore.instance.collection('steps');
+
     StepModel stepModel = StepModel.fromJson(
         (await steps.doc(uid).get()).data() as Map<String, dynamic>);
     return stepModel;
