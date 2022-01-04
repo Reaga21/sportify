a130ef3a79aa9b85528c3bbcb048f9c2032912f6
andrea.robitzsch <andrea.robitzsch@student.fh-kiel.de>
2022-01-04 18:01:01 +0100

    added a second dummy chart

--- lib/src/views/home/tabs/statistics/statistic_page.dart
+++ lib/src/views/home/tabs/statistics/statistic_page.dart
@@ -1,12 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:stats/stats.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 import 'package:sportify/src/util/dates.dart';
-import 'package:intl/intl.dart';
+
 
 class StatisticPage extends StatefulWidget {
   const StatisticPage({Key? key}) : super(key: key);
@@ -33,52 +30,68 @@ class _StatisticPageState extends State<StatisticPage> {
         title: const Text("Sportify"),
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20.0),
-            child: Text(
-              "Statistics",
-              style: TextStyle(
-                fontSize: 30,
-                color: Theme.of(context).colorScheme.secondary,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          FutureBuilder(
-              future: readData(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<Map<String, dynamic>> dataSteps) {
-                if (dataSteps.hasError) {
-                  return const Text("A problem has occurred!");
-                }
-                if (!dataSteps.hasData) {
-                  return const CircularProgressIndicator();
-                }
+      body: Center(
 
-                //dataSteps.data!['keyword']; // so kommt man an die Daten ran
-                //
-                //Map<String, dynamic> realData = dataSteps.data!;
-                return SfCartesianChart(
-                    series: <ChartSeries>[
-                      BarSeries<StepsData, dynamic>(
-                          dataSource: getChartData(dataSteps.data!),
-                          xValueMapper: (StepsData data, _) =>
-                              shortDate(data.date),
-                          yValueMapper: (StepsData data, _) => data.steps),
-                    ],
-                    primaryXAxis: CategoryAxis(),
-                    primaryYAxis: NumericAxis(
-                        edgeLabelPlacement: EdgeLabelPlacement.shift));
-              })
-        ],
+        child:SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Text(
+                  "Statistics",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              FutureBuilder(
+                  future: readData(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<Map<String, dynamic>> dataSteps) {
+                    if (dataSteps.hasError) {
+                      return const Text("A problem has occurred!");
+                    }
+                    if (!dataSteps.hasData) {
+                      return const CircularProgressIndicator();
+                    }
+                    return Column(
+                      children: [
+                        SfCartesianChart(
+                            series: <ChartSeries>[
+                              BarSeries<StepsData, dynamic>(
+                                  dataSource: getChartDataSeven(dataSteps.data!),
+                                  xValueMapper: (StepsData data, _) =>
+                                      shortDate(data.date),
+                                  yValueMapper: (StepsData data, _) => data.steps),
+                            ],
+                            primaryXAxis: CategoryAxis(),
+                            primaryYAxis: NumericAxis(
+                                edgeLabelPlacement: EdgeLabelPlacement.shift)),
+                        SfCartesianChart(
+                            series: <ChartSeries>[
+                              BarSeries<StepsData, dynamic>(
+                                  dataSource: getChartDataThirty(dataSteps.data!),
+                                  xValueMapper: (StepsData data, _) =>
+                                      shortDate(data.date),
+                                  yValueMapper: (StepsData data, _) => data.steps),
+                            ],
+                            primaryXAxis: CategoryAxis(),
+                            primaryYAxis: NumericAxis(
+                                edgeLabelPlacement: EdgeLabelPlacement.shift)),
+                      ],
+                    );
+                  })
+            ],
+          ),
+        ),
       ),
     );
   }
 
-  List<StepsData> getChartData(Map<String, dynamic> dataSteps) {
+  List<StepsData> getChartDataSeven(Map<String, dynamic> dataSteps) {
     final List<StepsData> chartData = [];
 
     for (MapEntry entry in dataSteps.entries) {
@@ -91,6 +104,21 @@ class _StatisticPageState extends State<StatisticPage> {
     }
     return chartData;
   }
+
+  List<StepsData> getChartDataThirty(Map<String, dynamic> dataSteps) {
+    final List<StepsData> chartData = [];
+
+    for (MapEntry entry in dataSteps.entries) {
+      chartData.add(StepsData(
+          DateTime.parse(entry.key), entry.value['stepsDay'])); //DATUM
+    }
+    chartData.sort((a, b) => a.date.compareTo(b.date));
+    if (chartData.length > 30) {
+      chartData.removeRange(0, chartData.length - 30);
+    }
+    return chartData;
+  }
+
 }
 
 class StepsData {
